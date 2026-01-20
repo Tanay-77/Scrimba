4bf5627681f6697d0a7d0334543fc3e68c74b061
Tanay Mahajan <tanaymahajan7@gmail.com>
2026-01-20 20:29:08 +0530

    new file

--- notes.js
+++ notes.js
@@ -1,3 +1,7 @@
-function tanay(){
-    console.log("This is the notes.js file.");
-}
+// function tanay(){
+//     console.log("This is the notes.js file.");
+// }
+ 
+// let array = ["tanay",20];
+// array.push("hello");
+// console.log(array);
