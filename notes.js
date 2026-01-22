a883b995d30defbafbf140d31cd48c4c1225c2f8
Tanay Mahajan <tanaymahajan7@gmail.com>
2026-01-22 19:03:00 +0530

    math.random

--- notes.js
+++ notes.js
@@ -9,3 +9,13 @@
 // for(let i=0; i<10; i++){
 //     console.log(i);
 // }
+
+// return 
+// Primary Purpose: To output a result from a function so it can be used elsewhere in the code, such as in variables, calculations, or other functions. 
+// Without return: A function returns undefined by default, meaning no useful value is passed back. 
+
+// math.random 
+// let num = Math.random(); // Generates a random number between 0 (inclusive) and 1 (exclusive)
+// let num = Math.random() * 10; // Generates a random number between 0 and 10
+// let num = Math.floor(Math.random() * 10); // Generates a random integer between 0 and 9
+// let num = Math.floor(Math.random() * 10) + 1; // Generates a random integer between 1 and 10
