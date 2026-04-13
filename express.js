7bdab0bcd366082eeb846043284a65bd3f556d57
Tanay Mahajan <tanaymahajan7@gmail.com>
2026-04-13 23:08:52 +0530

    query param

--- express.js
+++ express.js
@@ -1,12 +1,25 @@
-// always write type:module Enables import/export syntax: Allows you to break code into reusable, organized modules.
+// // always write type:module Enables import/export syntax: Allows you to break code into reusable, organized modules.
 
-import express from "express"
+// import express from "express"
+// import {celbrity} from data.js
 
-const PORT = 8000
+// const PORT = 8000
 
-const app = express
+// const app = express
 
-const celebrity ={
-    type:"hero",
-    name:"uday"
-}
+// const celebrity ={
+//     type:"hero",
+//     name:"uday"
+// }
+
+// we  can aceess date using this
+// app.get('/',(req,res)=>{
+//     res.json(celebrity)
+// })
+
+// app.listen(PORT,()=> console.log("done"))
+
+// Quey param (means accesing anything by typing stuf)
+
+// app.get('/',(req,res)=>{
+//         })
